4a62ef6c5f539fd3eae777ce42061fa82507b5ba
tako <rina.m.takase@gmail.com>
2017-07-30 01:32:12 +0900

    Implement calculate resources

--- app/controllers/calculate_controller.rb
+++ app/controllers/calculate_controller.rb
@@ -1,4 +1,41 @@
 class CalculateController < ApplicationController
+  before_action :set_metrix, only: [:sum, :prod]
   def index
+  end
+
+  def sum
+    sum_metrix = MetrixService.sum(@metrix_a, @metrix_b)
+    if sum_metrix.error
+      @error = sum_metrix.error
+    else
+      @sum = sum_metrix.c.transpose
+    end
+    render :index
+  end
+
+  def prod
+    prod_metrix = MetrixService.prod(@metrix_a, @metrix_b)
+    if prod_metrix.error
+      @error = prod_metrix.error
+    else
+      @prod = prod_metrix.c.transpose
+      p @prod
+    end
+    render :index
+  end
+
+  private
+
+  def set_metrix
+    @metrix_a = MetrixService.new(build_metrix(params[:metrix_a]))
+    @metrix_b = MetrixService.new(build_metrix(params[:metrix_b]))
+  end
+
+  def build_metrix(_metrix)
+    metrix = []
+    _metrix.split("\r\n").each do |row|
+      metrix << row.split(' ').map(&:to_i)
+    end
+    metrix
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  root 'calculate#index'
+
   get 'calculate/index'
+  post 'calculate/sum'
+  post 'calculate/prod'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
